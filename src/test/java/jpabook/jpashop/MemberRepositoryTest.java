8dfc3d19b01f8bc59ca0bbd7b9cb17eade7d7380
HanGyulKang <gang6607@gmail.com>
2022-04-08 00:38:16 +0900

    2022 04 08

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class) // Spring과 관련된 테스트를 하는 것을 JUnit에 알려주는 느낌..
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional   // Transaction은 Spring것을 사용하길 권장(옵션이 많음) Entity를 활용하는 것은 무조건 Transaction이 있어야 함, 그렇지 않으면 에러 발생
+    @Rollback(false) // Transactional 어노테이션이 Test케이스에 있다면 전부 Rollback하기 때문에 데이터가 남진 않음
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
