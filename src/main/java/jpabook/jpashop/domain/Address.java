8087e19b138832e38c4da57e8d6ab38cbf7c2355
HanGyulKang <gang6607@gmail.com>
2022-04-10 22:12:47 +0900

    2022 04 10

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -7,10 +7,20 @@ import javax.persistence.Embeddable;
 
 @Embeddable // 내장타입
 @Getter
-@Setter
 public class Address {
 
+    // 값 타입은 변경 불가능하게 설계해야한다.
     private String city;
     private String street;
     private String zipcode;
+
+    // 함부로 new로 생성하면 안 되기 때문에 protected로 묶어놓는다.
+    // JPA 스펙에 나와있는 제약.
+    protected Address() {}
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
