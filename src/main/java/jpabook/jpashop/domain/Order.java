fa0585aa16db6b5127dc4cdf865ccd0e8d475180
HanGyulKang <gang6607@gmail.com>
2022-04-10 23:03:07 +0900

    2022 04 10

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -23,10 +23,20 @@ public class Order {
     @JoinColumn(name = "member_id") // FK 설정 : member테이블의 id
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    /*
+    -> non cascade
+    persist(orderItemA)
+    persist(orderItemB)
+    persist(orderItemC)
+    persist(order)
+
+    -> cascade
+    persist(order)
+     */
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -35,4 +45,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 [ORDER, CANCLE]
+
+    //==연관관계 편의 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOder(this);
+    }
 }
