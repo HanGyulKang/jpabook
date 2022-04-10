ce1191b88200b326683b7b9aab9fdf66692a57c4
HanGyulKang <gang6607@gmail.com>
2022-04-11 00:37:57 +0900

    2022 04 11

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -3,7 +3,7 @@ package jpabook.jpashop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // Component Scan을 하여 Spring Bean에 모두 등록
 public class JpashopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
