30eb46f1b54557f804582aefcb39e6d69132ce64
HanGyulKang <gang6607@gmail.com>
2022-04-15 00:28:13 +0900

    2022 04 15

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if(item.getId() == null) {
+            // 처음에 아이템이 없기 때문에 JPA가 제공하는 persist를 사용, 즉 완전히 새로 생성한 데이터(신규 등록)
+            em.persist(item);
+        } else {
+            // update와 유사함s
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
