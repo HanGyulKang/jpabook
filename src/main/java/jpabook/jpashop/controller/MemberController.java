dcef61580458ede6eb23e0d6c9beaf7d1e49575c
HanGyulKang <gang6607@gmail.com>
2022-04-17 21:34:50 +0900

    2022 04 17

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    // 등록 폼 화면을 열어보는 목적 : Get
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    // 멤버 정보를 등록하려는 목적 : Post
+    @PostMapping("members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        // Valid 체크한 것이 에러가 있다면...
+        if(result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
